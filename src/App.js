2332509168dd66cbddcf2ffd75750f76f54e152f
Reinis Riekstins <riekstins.reinis@gmail.com>
2017-01-06 16:12:46 +0200

    made table almost fully functional, 1 bug remains

--- src/App.js
+++ src/App.js
@@ -3,83 +3,111 @@ import { observer } from 'mobx-react'
 
 const Tr = observer(({ store, state, i }) => {
   const { title, year } = state
+  const { selected, isMouseDown } = store
 
-  const pushOrPopSelection = e => {
-    const tr = e.target.parentNode
-    const { isMouseDown, selected } = store
-
+  const toggleSelection = () => {
     if ( selected.includes(state) ) {
-        selected.splice( selected.indexOf(state, 1) )
-        tr.setAttribute('style', '')
-        console.log('popped', [state.title, state.year])
-      }
-      else {
-        selected.push(state)
-        console.log('pushed', [state.title, state.year])
-        tr.setAttribute('style', 'background-color: dodgerblue;')
-      }
-  }
-
-  const handleMouseOver = e => {
-    const { isMouseDown, selected } = store
-
-    if ( isMouseDown.value ) {
-      if ( selected.includes(state) ) {
-        selected.splice( selected.indexOf(state, 1) )
-        console.log('popped', [state.title, state.year])
-      }
-      else {
-        selected.push(state)
-        console.log('pushed', [state.title, state.year])
-      }
+      selected.splice( selected.indexOf(state), 1 )
+      console.table([{ action: 'added', title, year }])
+    }
+    else {
+      selected.push(state)
+      console.table([{ action: 'removed', title, year}])
     }
   }
 
+  const handleMouseOver = () => {
+    if (isMouseDown.value) toggleSelection()
+  }
+
   return (
-    <tr 
+    <tr
       onMouseOver={ handleMouseOver }
-      onMouseDown={ pushOrPopSelection }
-      style={ store.selected.includes(state) ? { backgroundColor: 'dodgerblue' } : {} }>
+      onMouseDown={ toggleSelection }
+      style={(() => {
+        if (selected.includes(state))
+          return { 
+            backgroundColor: 'dodgerblue', 
+            color: 'white'
+          }
+        return {}
+      })()}>
       <td>{ i }</td>
       <td>{ title }</td>
       <td>{ year }</td>
-      <td>false</td>
-      <td>{ store.selected.includes(state) ? 'true' : 'false' }</td>
+      <td>{ selected.includes(state) ? 'true' : 'false' }</td>
     </tr>
   )
 })
 
 const App = ({ store }) => {
+  const { isMouseDown, all, filter, visible, selected } = store
+
   const noSelect = {
-    WebkitTouchCallout: 'none',
-    WebkitUserSelect: 'none',
-    KhtmlUserSelect: 'none',
-    MozUserSelect: 'none',
-    MsUserSelect: 'none',
+    // WebkitTouchCallout: 'none',
+    // WebkitUserSelect: 'none',
+    // KhtmlUserSelect: 'none',
+    // MozUserSelect: 'none',
+    // MsUserSelect: 'none',
     userSelect: 'none'
+  }
+
+  const handleInputChange = e => {
+    filter.value = e.target.value
+
+    console.table(
+      selected
+      .slice()
+      .map(({title, year}) => 
+        ({ action: 'removed', title, year })
+      )
+    )
+    function emptyArray(arr) {
+      arr.pop()
+      if (arr.length) emptyArray(arr)
+    }
+
+    emptyArray(selected)
   }
 
   return (
     <div className="large-6 columns">
+      <div className="large-6 columns">
+      <label htmlFor="filter">
+        <strong>Filter:</strong>
+      </label>
+      <input type="text" id="filter" value={ filter.value } onChange={ handleInputChange } />
+      </div>
+      <div className="large-6 columns">
+        <strong>Selected: { selected.length }</strong>
+      </div>
       <table style={ noSelect }>
         <thead>
           <tr>
             <th>#</th>
             <th>Title</th>
             <th>Year</th>
-            <th>Selecting</th>
             <th>Selected</th>
           </tr>
         </thead>
         <tbody
           style={{ cursor: 'pointer' }} 
-          onMouseDown={ e => store.isMouseDown.value = true }
-          onMouseUp={ e => store.isMouseDown.value = false }>
-        {
-          store.all.map((x, i) => 
-            <Tr key={ i } store={ store } state={ store.all[i] } i={ i } />
+          onMouseDown={ e => isMouseDown.value = true }
+          onMouseUp={ e => isMouseDown.value = false }>
+        {(() => {
+          if (visible.length)
+            return visible.map((x, i) =>
+              <Tr key={ i } store={ store } state={ visible[i] } i={ i } />
+            )
+          else return (
+            <tr>
+              <td></td>
+              <td>None found.</td>
+              <td></td>
+              <td></td>
+            </tr>
           )
-        }
+        })()}
         </tbody>
       </table>
     </div>
--- src/store.js
+++ src/store.js
@@ -1,20 +1,41 @@
 import { observable, computed } from 'mobx'
+import { Observable } from 'rxjs/Rx'
+import { toStream, fromStream } from 'mobx-utils'
 import difference from 'lodash/fp/difference'
+import last from 'lodash/fp/last'
+
 import data from './sample-data'
 
 export const store = {
+  filter: observable({ value: '' }),
   isMouseDown: observable(false),
   all: observable(data),
-  selected: observable([
+  // filtered results
+  get visible() {
+   return store.all.filter(x => {
+      const { title, year } = x
+      const { value } = store.filter
+      const filterRegex = new RegExp(value, 'gi')
 
-  ]),
-  selecting: observable([
-    
-  ]),
-  chosen: observable([
-
-  ]),
-  get unchosen() {
-    return difference(store.all, store.chosen)
+      if (filterRegex.test(title) || filterRegex.test(year))
+        return true
+    })
+  },
+  selected: observable([]),
+  get unselected() {
+    return difference(store.all, store.selected)
   }
 }
+
+const lastSelected$ = Observable
+  .from(toStream(() => last(store.selected)))
+
+const lastUnselected$ = Observable
+  .from(toStream(() => last(store.unselected)))
+
+const lastToggleSelected$ = Observable
+  .merge(lastSelected$, lastUnselected$)
+
+store.lastToggleSelected = computed(() => {
+  return fromStream(lastToggleSelected$)
+})
