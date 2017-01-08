385c8962fdd5842ae00cb65b5ad405740741099c
Reinis Riekstins <rr14022@lu.lv>
2017-01-08 15:30:08 +0200

    changed store to default export

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-import { store } from './store'
+import store from './store'
 
 import './foundation.css'
 import './index.css'
